8e9c9a448c335b5f66f6cc537234983a222aeefb
Maximilian Hans <hans.maximilian@icloud.com>
2024-05-03 21:30:12 +0200

    clean
    delete everything for a clean start for tutorai2024

--- Backend/prompt_engineering.py
+++
@@ -1,5 +0,0 @@
-def structured_input(prompt,transformer):
-    Frage = "Frage:"
-    Quelle = "Quelle"
-    prompt_eng = "beantworte nur die Frage die ich gestellt habe" 
-    return Frage + prompt +  transformer + prompt_eng  
