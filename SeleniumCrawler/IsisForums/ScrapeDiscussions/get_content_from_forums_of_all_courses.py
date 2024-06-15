0ce5d87565718e918ed10599983d8ed851c8593d
KostaIBravo <82755437+KostaIBravo@users.noreply.github.com>
2024-06-15 11:33:23 +0200

    Update get_content_from_forums_of_all_courses.py

--- SeleniumCrawler/IsisForums/ScrapeDiscussions/get_content_from_forums_of_all_courses.py
+++ SeleniumCrawler/IsisForums/ScrapeDiscussions/get_content_from_forums_of_all_courses.py
@@ -9,21 +9,24 @@ from selenium.common.exceptions import NoSuchElementException
 from get_content_from_forums_of_one_course import get_content_from_forums_of_one_course
 
 def get_content_from_forums_of_all_courses(driver):
-    course_dict = []
+    #course_dict = []
     
     # Load course data from JSON file
-    with open('course_id_saved.json','r') as data:
+    with open('my_courses_id.json','r') as data:
         course_data = json.load(data)
         
     # Iterate through each course
     for course in course_data:
         try:
+            file_name = "course_"+course
+            
             # Retrieve forum content for the current course
             course_forums_content = get_content_from_forums_of_one_course(course, driver)
-            course_dict.append(course_forums_content)
+            #course_dict.append(course_forums_content)
+            with open(f'course_forum_data/{file_name}.json', 'w') as c:
+                json.dump(course_forums_content, c, ensure_ascii=False, indent=4)
+
         except Exception as e:
             print(f"Failed to retrieve forum content for course {course}: {e}")
     
-    # Save results to a new JSON file
-    with open('all_courses.json', 'w') as c:
-        json.dump(course_dict, c, ensure_ascii=False, indent=4)
+   
