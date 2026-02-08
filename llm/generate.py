98a931e60c62dbfd661e76582e44ab966bea02e4
toni.buch <a.buchholz.1@campus.tu-berlin.de>
2026-02-08 16:38:26 +0100

    updated sys prompt

--- llm/generate.py
+++ llm/generate.py
@@ -1,2 +1,5 @@
 #TODO provider agnostic LLM generation 
 #called by bot/handlers.py delegates to llm/ollama.py
+
+
+#Attention!!! Currently we only have Ollama, so this file is unused, not to say obsolete
